49a208ec91a41eac680457b756effa77744e1e98
mizzadnan <mirza.adnan2205@gmail.com>
2021-03-04 23:59:19 +0600

    added function for opening and closing popup

--- script.js
+++ script.js
@@ -7,4 +7,22 @@ let Book = function(title, author, pages, read) {
     this.read = read;
 }
 
+// opening and closing the popup
+const popup = document.querySelector("#popup-bg")
+const addButtons = document.querySelectorAll(".add");
+addButtons.forEach(button => {
+    button.addEventListener("click", function() {
+        togglePopup("flex");
+    })
+})
+
+const closeButton = document.querySelector("#close-button");
+closeButton.addEventListener("click", function() {
+    togglePopup("none");
+})
+
+function togglePopup(display) {
+    popup.style.display = display;
+}
+
 
